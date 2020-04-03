abbb1139c18627f8717dd32936d98e72fe7485d7
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-04-03 21:34:20 +0300

    Create cross-reference-generator

---
+++ cross-reference-generator/target/debug/cross-reference-generator.d
@@ -0,0 +1,1 @@
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\cross-reference-generator.pdb: D:\code\workSpace\cross-reference-generator\cross-reference-generator\src\main.rs
---
+++ cross-reference-generator/target/debug/deps/cross_reference_generator-ece709e0c7bf6a61.d
@@ -0,0 +1,5 @@
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\deps\cross_reference_generator-ece709e0c7bf6a61.exe: src\main.rs
+
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\deps\cross_reference_generator-ece709e0c7bf6a61.d: src\main.rs
+
+src\main.rs:
