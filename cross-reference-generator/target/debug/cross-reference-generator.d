708659c1551c24fc0e5fc1764ad55b294faed600
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-04-04 19:18:27 +0300

    Update cross-reference-generator

---
+++ cross-reference-generator/target/debug/cross-reference-generator.d
@@ -0,0 +1,1 @@
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\cross-reference-generator.pdb: D:\code\workSpace\cross-reference-generator\cross-reference-generator\src\files_io.rs D:\code\workSpace\cross-reference-generator\cross-reference-generator\src\main.rs
---
+++ cross-reference-generator/target/debug/deps/cross_reference_generator-ece709e0c7bf6a61.d
@@ -0,0 +1,6 @@
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\deps\cross_reference_generator-ece709e0c7bf6a61.exe: src\main.rs src\files_io.rs
+
+D:\code\workSpace\cross-reference-generator\cross-reference-generator\target\debug\deps\cross_reference_generator-ece709e0c7bf6a61.d: src\main.rs src\files_io.rs
+
+src\main.rs:
+src\files_io.rs:
